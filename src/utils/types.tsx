dd747368dbdf1ff2efcb6dc8f3075290cad6aea4
maria-vaganova <vaganova.m.a@yandex.ru>
2024-11-25 07:29:35 +0400

    Add data mock

---
+++ src/utils/types.tsx
@@ -0,0 +1,14 @@
+export interface Ingredient {
+    id: string;
+    name: string;
+    type: string;
+    proteins:number;
+    fat:number;
+    carbohydrates:number;
+    calories:number;
+    price:number;
+    image: string;
+    image_mobile: string;
+    image_large: string;
+    version: number;
+}
